527f99d29417fc1bc9ea8815c44f4932ba0fd026
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-26 13:31:16 +0100

    Cleaned and commented the code a little bit

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,46 +1,44 @@
 function wc_inputOutput() {
 
-    let wc_inputTextOriginal = document.getElementById("wc-text-input").value
+    let wc_inputText = document.getElementById("wc-text-input").value
 
-    let wc_inputText
-    if (wc_inputTextOriginal[wc_inputTextOriginal.length - 1] == " ") {
-        wc_inputText = wc_inputTextOriginal.substr(0, wc_inputTextOriginal.length - 1)
-    } else {
-        wc_inputText = wc_inputTextOriginal
+    // Remove the space at the end of the text if there is one
+
+    if (wc_inputText[wc_inputText.length - 1] == " ") {
+        wc_inputText = wc_inputText.substr(0, wc_inputText.length - 1)
     }
-
+    
+    // Warning if there is no input text
 
     if (wc_inputText == "") {
         document.getElementById('wc-words-count-output').innerHTML = "Please enter a text."
     } else {
 
-        // words
+        // count words
 
         let words = wc_inputText.split(' ')
-
+        
         let wordCount = words.length + 1;
         wordCount--
 
         let resultWords = 'words: ' + wordCount
 
 
-        // sentences
+        // count sentences (sentences, statements, exclamations and questions)
 
         let statements = wc_inputText.split('.')
-        
         let exclamations = wc_inputText.split('!')
-
         let questions = wc_inputText.split('?')
 
         let statementsCount = statements.length;
         statementsCount--
-        
+
         let exclamationsCount = exclamations.length;
         exclamationsCount--
 
         let questionsCount = questions.length;
         questionsCount--
-        
+
         let sentencesCount = statementsCount + exclamationsCount + questionsCount;
 
         let resultSentences = 'Sentences: ' + sentencesCount
@@ -48,49 +46,36 @@ function wc_inputOutput() {
         let resultExclamations = 'exclamations: ' + exclamationsCount
         let resultQuestions = 'questions: ' + questionsCount
 
-
-        // charcters
+        // count charcters
 
         let charactersCount = wc_inputText.length;
-
         let resultCharacters = 'characters: ' + charactersCount
 
-
-        // characters without spaces
+        // count characters without spaces
 
         let charactersCountWithoutSpace = wc_inputText.replace(/ /g, ""); // remove whitespace
-
         let charactersXCount = charactersCountWithoutSpace.length;
-
         let resultXCharacters = 'characters without spaces: ' + charactersXCount
-
-
 
         // count vowels
 
         const countVowels = (str) => str.split("").filter(letter => 'AEIOUÄÖÜaeiouäöü'.includes(letter)).length
         let resultVowels = 'Vowels: ' + countVowels(wc_inputText)
 
-
         // count consonants
 
         const countConsonants = (str) => str.split("").filter(letter => 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyzß'.includes(letter)).length
         let resultConsonants = 'Consonants: ' + countConsonants(wc_inputText)
-        
-        
+
         // count digits
 
         const countDigits = (str) => str.split("").filter(letter => '0123456789'.includes(letter)).length
         let resultDigits = 'Digits: ' + countDigits(wc_inputText)
 
-
-
         // output
 
         document.getElementById('wc-words-count-output').innerHTML = `${resultWords} <br> ${resultCharacters} <br> ${resultXCharacters} <br> ${resultSentences} (${resultStatements}, ${resultExclamations}, ${resultQuestions}) <br> ${resultVowels} <br> ${resultConsonants} <br> ${resultDigits}`
-
     }
 }
-
 
 document.querySelector('#wc-counter-button').addEventListener('click', wc_inputOutput);
